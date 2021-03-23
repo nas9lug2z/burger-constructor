b2ee85f51d9a7ac980b01c2b544f106e37b461e3
Ana <64069789+nas9lug2z@users.noreply.github.com>
2021-03-23 17:38:04 +0100

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, withErrorHandler, Fragment } from 'react';
+import React, { Component, withErrorHandler, Fragment, Suspense } from 'react';
 import { Route, Switch, Redur } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -6,10 +6,18 @@ import * as actions from './store/actions/index';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilderContainer from './containers/BurgerBuilderPage/BurgerBuilderContainer';
-import Checkout from './containers/Checkout/Checkout';
-import ContactData from './containers/Checkout/ContactData/ContactData';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+// import Checkout from './containers/Checkout/Checkout';
+// import ContactData from './containers/Checkout/ContactData/ContactData';
+
+// import Auth from './containers/Auth/Auth';
+import Spinner from './components/UI/Spinner/Spinner';
+// import Orders from './containers/Orders/Orders';
+const Orders = React.lazy(_ => import('./containers/Orders/Orders'));
+const Auth = React.lazy(_ => import('./containers/Auth/Auth'));
+const Checkout = React.lazy(_ => import('./containers/Checkout/Checkout'));
+const ContactData = React.lazy(_ =>
+	import('./containers/Checkout/ContactData/ContactData')
+);
 
 class App extends Component {
 	componentDidMount() {
@@ -20,22 +28,32 @@ class App extends Component {
 		return (
 			<Fragment>
 				<Layout>
-					<Switch>
-						<Route exact path='/orders' component={Orders} />
-						<Route exact path='/auth' component={Auth} />
-						{this.props.authenticated && this.props.purchasable !== 0 ? (
+					<Suspense fallback={<Spinner />}>
+						<Switch>
 							<Route
 								exact
-								path='/checkout/contact-data'
-								component={ContactData}
+								path='/orders'
+								render={props => <Orders {...props} />}
 							/>
-						) : null}
-						{this.props.authenticated && this.props.purchasable !== 0 ? (
-							<Route exact path='/checkout' component={Checkout} />
-						) : null}
-						<Route exact path='/' component={BurgerBuilderContainer} />
-						<Route render={_ => <h1>404 Not Found</h1>} />
-					</Switch>
+							<Route exact path='/auth' render={props => <Auth {...props} />} />
+							{this.props.authenticated && this.props.purchasable !== 0 ? (
+								<Route
+									exact
+									path='/checkout/contact-data'
+									render={props => <ContactData {...props} />}
+								/>
+							) : null}
+							{this.props.authenticated && this.props.purchasable !== 0 ? (
+								<Route
+									exact
+									path='/checkout'
+									render={props => <Checkout {...props} />}
+								/>
+							) : null}
+							<Route exact path='/' component={BurgerBuilderContainer} />
+							<Route render={_ => <h1>404 Not Found</h1>} />
+						</Switch>
+					</Suspense>
 				</Layout>
 			</Fragment>
 		);
